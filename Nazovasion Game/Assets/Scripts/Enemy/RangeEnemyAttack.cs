b2498e337cd938c7b4dbae651439a7d7611aca70
Nixon Scherbarth <nixonbusinessmail@gmail.com>
2022-12-05 22:31:19 +0100

    Player takes Damage from shooting enemy.

---
+++ Nazovasion Game/Assets/Scripts/Enemy/RangeEnemyAttack.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEnemyAttack : MonoBehaviour
+{
+    EnemyController refScript;
+    PlayerHealth playerHealth;
+    [SerializeField] GameObject player;
+
+    public float speed;
+    public Transform target;
+    public float shotDistance;
+    public float minimumDistance;
+    public float maximumDistance;
+
+    public GameObject projectile;
+    public float timeBetweenShots;
+    private float nextShotTime;
+
+    private void Start()
+    {
+        refScript = GetComponent<EnemyController>();
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+    private void Update()
+    {
+        if (Time.time > nextShotTime)
+        {
+            if (Vector2.Distance(transform.position, target.position) < shotDistance)
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                nextShotTime = Time.time + timeBetweenShots;
+            }
+            if (Vector2.Distance(transform.position, target.position) > maximumDistance)
+            {
+                StartCoroutine(MoveTowards());
+            }
+        }
+        if (Vector2.Distance(transform.position, target.position) < minimumDistance)
+        {
+            StartCoroutine(MoveAway());
+        }
+    }
+    IEnumerator MoveTowards()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        yield return new WaitForSeconds(1f);
+    }
+    IEnumerator MoveAway()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+        yield return new WaitForSeconds(1f);
+    }
+}
---
+++ Nazovasion Game/Assets/Scripts/Weapon/Enemybullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemybullet : MonoBehaviour
+{
+    Vector3 targetPosition;
+    public float speed;
+    private void Start()
+    {
+        targetPosition = FindObjectOfType<PlayerController>().transform.position;
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+            collision.GetComponent<PlayerHealth>().UpdateHealth(-5);
+        }
+    }
+}
